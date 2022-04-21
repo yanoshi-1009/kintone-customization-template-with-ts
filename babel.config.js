6a3b72764d7f0cd8b0e2fdecc217166f8edf5ce7
Shohei Yano <y.idakko@gmail.com>
2022-04-21 21:48:27 +0900

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = function (api) {
       "@babel/preset-env",
       {
         useBuiltIns: "usage",
-        corejs: "3.20"
+        corejs: 3
       }
     ],
     "@babel/preset-typescript"
